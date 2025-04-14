33a16136c5ee166dbb406d8621e851f3fb220798
kanika-sharma-pro <kanikasharma1908@gmail.com>
2025-04-14 13:55:36 -0400

    working

--- src/main/java/io/zipcoder/GroceryReporter.java
+++ src/main/java/io/zipcoder/GroceryReporter.java
@@ -2,8 +2,10 @@ package io.zipcoder;
 
 import io.zipcoder.utils.FileReader;
 
+import java.util.Arrays;
+
 public class GroceryReporter {
-    private final String originalFileText;
+    private final String  originalFileText;
 
     public GroceryReporter(String jerksonFileName) {
         this.originalFileText = FileReader.readFile(jerksonFileName);
@@ -11,6 +13,27 @@ public class GroceryReporter {
 
     @Override
     public String toString() {
-        return null;
+
+return null;
+    }
+
+
+    public String getOriginalFileText() {
+        return originalFileText;
     }
 }
+//public static String reverseFirstWordThenCamelCase(String sentence) {
+//        String [] words = sentence.split("\\W+");
+//        String first = words [0];
+//        String reverse = new StringBuilder(first).reverse().toString();
+//        return reverse.substring(0, 1).toUpperCase() + reverse.substring(1);
+//    }
+//public String split(String regex)
+//
+//public String split(String regex, int limit)
+// public String getOriginalFileText() {
+//        return originalFileText;
+//    }
+//String [] filename = originalFileText.split("\\##+");
+//String jerksonFileName = "Milk##Bread##Cookies##Apple";
+//        String items = Arrays.toString(jerksonFileName.split("##"));
--- src/main/java/io/zipcoder/ItemParser.java
+++ src/main/java/io/zipcoder/ItemParser.java
@@ -3,14 +3,26 @@ package io.zipcoder;
 import io.zipcoder.utils.Item;
 import io.zipcoder.utils.ItemParseException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ItemParser {
     public List<Item> parseItemList(String valueToParse) {
-        return null;
+        List<Item> itemList = new ArrayList<>();
+        String [] tempArray = valueToParse.split("##");
+        for (String string : tempArray) {
+            try {
+                itemList.add(parseSingleItem(string + "##"));
+            } catch (ItemParseException e) {
+
+            }
+        }
+        return itemList;
     }
 
     public Item parseSingleItem(String singleItem) throws ItemParseException {
-        return null;
+
+        Item itemToReturn = new Item(parseName(String singleItem), parsePrice(singleItem), parseType(single Item), parseData(singleItem) )
+        return itemToReturn;
     }
 }
