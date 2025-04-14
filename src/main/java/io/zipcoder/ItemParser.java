fe56f3d0bfa977ddc847dd5ca243014def133fed
kanika-sharma-pro <kanikasharma1908@gmail.com>
2025-04-14 15:01:13 -0400

    ParsedSingleItem and PaParseBrokenSingleItemTest, both passed

--- src/main/java/io/zipcoder/ItemParser.java
+++ src/main/java/io/zipcoder/ItemParser.java
@@ -5,15 +5,21 @@ import io.zipcoder.utils.ItemParseException;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
-public class ItemParser {
+
+ public class ItemParser {
+    private int errorCount = 0;
+
     public List<Item> parseItemList(String valueToParse) {
         List<Item> itemList = new ArrayList<>();
-        String [] tempArray = valueToParse.split("##");
-        for (String string : tempArray) {
+        String [] itemString = valueToParse.split("##");
+        for (String itemstring : itemString) {
             try {
-                itemList.add(parseSingleItem(string + "##"));
+                itemList.add(parseSingleItem(itemString + "##"));
             } catch (ItemParseException e) {
+                e.printStackTrace();
+                errorCount++;
 
             }
         }
@@ -21,8 +27,37 @@ public class ItemParser {
     }
 
     public Item parseSingleItem(String singleItem) throws ItemParseException {
+String name = null;
+String type = null;
+String expiration = null;
+Double price = null;
+        Pattern i = Pattern.compile(";");
+        Pattern nameValuePattern = Pattern.compile("[@:\\^*%]");
+        singleItem = singleItem.substring(0, singleItem.length() - 2);
 
-        Item itemToReturn = new Item(parseName(String singleItem), parsePrice(singleItem), parseType(single Item), parseData(singleItem) )
-        return itemToReturn;
+        String[] fieldString = i.split(singleItem);
+        for (String fieldStrings : fieldString ) {
+            String[] nameValues = nameValuePattern.split(fieldStrings);
+            if (nameValues[0].equalsIgnoreCase("name")) {
+                name = nameValues.length > 1 && nameValues[1] != null ? nameValues[1].toLowerCase() : null;
+            } else if(nameValues[0].equalsIgnoreCase("type")) {
+                type = nameValues.length > 1 && nameValues[1] != null ? nameValues[1].toLowerCase() : null;
+            } else if (nameValues[0].equalsIgnoreCase("expiration")){
+                expiration = nameValues.length > 1 && nameValues[1] != null ? nameValues[1].toLowerCase() : null;
+            } else if ((nameValues[0].equalsIgnoreCase("price"))) {
+                price = nameValues.length > 1 && nameValues[1] != null ? Double.parseDouble(nameValues[1]) : null;
+            }
+        }
+        if ( name == null || type == null || expiration == null || price == null) {
+            throw new ItemParseException();
+        }
+        //Item itemToReturn = new Item(parse(), parsePrice(singleItem), parseType(singleItem), parseData(singleItem) );
+        return new Item (name, price, type, expiration);
     }
+
+public int getErrorCount() {
+    return errorCount;
+}
+
+
 }
